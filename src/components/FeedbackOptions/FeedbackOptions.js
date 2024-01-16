e7be0a9c32533381f4074f9f9ff1c951befe1d36
Viktoriia <128598090+kamineckav@users.noreply.github.com>
2024-01-16 14:14:25 +0100

    finished click on the buttons

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FeedbackOptions.module.css';
+import PropTypes from 'prop-types';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  const [clickedButton, setClickedButton] = useState(null);
+
+  const handleMouseDown = (option) => {
+    setClickedButton(option);
+  };
+
+  const handleMouseUp = (option) => {
+    setClickedButton(null);
+    onLeaveFeedback(option);
+  };
+
   return (
     <div>
-      {options.map(option => (
+      {options.map((option) => (
         <button
           key={option}
-          className={`${styles['feedback-btn']} ${styles[option]}`}
-          onClick={() => onLeaveFeedback(option)}
+          type="button"
+          className={`${styles['feedback-btn']} ${styles[option]} ${
+            clickedButton === option ? styles.clicked : ''
+          }`}
+          onMouseDown={() => handleMouseDown(option)}
+          onMouseUp={() => handleMouseUp(option)}
+          onMouseLeave={() => setClickedButton(null)} // Додано обробник події при відведенні миші
         >
           {option}
         </button>
       ))}
     </div>
   );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
 
 export default FeedbackOptions;
