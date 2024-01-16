0da03f6914b45a57a247e08c03cbcb3bf63da6ef
Viktoriia <128598090+kamineckav@users.noreply.github.com>
2024-01-16 13:42:39 +0100

    finish task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
+import React, { useState } from 'react';
+import Statistics from '../components/Statistics/Statistics';
+import FeedbackOptions from '../components/FeedbackOptions/FeedbackOptions';
+import Section from '../components/Section/Section';
+import Notification from '../components/Notification/Notification';
+import styles from './App.module.css';
+import '../index.css'
+
+const App = () => {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  const [showNotification, setShowNotification] = useState(true);
+
+  const onLeaveFeedback = (type) => {
+    setState((prevState) => ({ ...prevState, [type]: prevState[type] + 1 }));
+    setShowNotification(false);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.container}>
+      <Section title="Leave Feedback">
+        <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={onLeaveFeedback} />
+      </Section>
+
+      <Section title="Statistics">
+        {state.good + state.neutral + state.bad > 0 ? (
+          <Statistics
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
+            total={state.good + state.neutral + state.bad}
+            positivePercentage={
+              state.good > 0
+                ? Math.round((state.good / (state.good + state.neutral + state.bad)) * 100)
+                : 0
+            }
+          />
+        ) : (
+          showNotification && <Notification message="There is no feedback" />
+        )}
+      </Section>
     </div>
   );
 };
+
+export default App;
+
---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map(option => (
+        <button
+          key={option}
+          className={`${styles['feedback-btn']} ${styles[option]}`}
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default FeedbackOptions;
